ff0781372c8fdc4cdf1b56391ecc591fbbb9b838
Shreyaaa24 <shreyaaa2464@gmail.com>
2024-10-28 01:37:01 +0530

    changes

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,36 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Cartitems from '../Components/Cartitems/Cartitems'
-import { UserCircleIcon } from '@heroicons/react/16/solid'
-import ShopContext from '../Context/ShopContext'
-import remove_icon from '../Assets/cart_cross_icon.png'
-const Cart = () => {
 
-  const {all_product,Cartitems,removefromcart}=useContext(ShopContext)
+const Cart = () => {
   return (
-    <div className='cartitems'>
-      <div className='cartitems-format-main'>
-        <p>PRODUCT</p>
-        <p>TITLE</p>
-        <p>PRICE</p>
-        <p>QUANTITY</p>
-        <p>TOTAL</p>
-        <p>REMOVE</p>
-      </div>
-      <hr/>
-      <div>
-        <div className='cartitems-format'>
-          <img src =""alt="" className='carticon-product-icon'/>
-<p></p>
-<p></p>
-<button className='cartitems-quantity'></button>  
-<p></p>      
-<img src={remove_icon} onClick={()=>{removefromcart()}}alt=""/>
-    </div>
-    <hr/>
-    {all_product.map((e)=>{
-if(Cartitems[e.id>0])
-    })}
-      </div>
+    <div>
+      <Cartitems/>
     </div>
   )
 }
